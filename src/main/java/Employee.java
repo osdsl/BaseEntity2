167d85b7f98ea2264c303b896ffb52fd5bf83ad5
Vyacheslav <119933448+osdsl@users.noreply.github.com>
2024-02-29 18:26:53 +0700

    Update Employee.java

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -9,13 +9,13 @@ public class Employee extends Person {
         this.uniqueBoolean = false;
         this.anotherUniqueString = "defaultAnotherUniqueString";
     }
-
+//Конструктор для создания объектов класса Employee с заданными значениями полей
     public Employee(int id, String name, boolean uniqueBoolean, String anotherUniqueString) {
         super(id, name);
         this.uniqueBoolean = uniqueBoolean;
         this.anotherUniqueString = anotherUniqueString;
     }
-
+//Методы доступа  позволяют получить и установить значения уникальных полей класса Employee
     public boolean isUniqueBoolean() {
         return uniqueBoolean;
     }
@@ -31,7 +31,9 @@ public class Employee extends Person {
     public void setAnotherUniqueString(String anotherUniqueString) {
         this.anotherUniqueString = anotherUniqueString;
     }
-
+// Метод  переопределен для сравнения объектов класса Employee.
+// Он сначала использует метод equals суперкласса Person, 
+// а затем сравнивает уникальные поля uniqueBoolean и anotherUniqueString
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -41,12 +43,12 @@ public class Employee extends Person {
         return uniqueBoolean == that.uniqueBoolean &&
                 Objects.equals(anotherUniqueString, that.anotherUniqueString);
     }
-
+// Метод переопределен для вычисления хэш-кода объекта Employee на основе значений всех его полей
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), uniqueBoolean, anotherUniqueString);
     }
-
+// Метод переопределен для представления информации об объекте класса Employee в виде строки
     @Override
     public String toString() {
         return "Employee{" +
